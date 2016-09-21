3496e083753b7ed04a9e1c027352b16b1ff9f8cc
john mailey <j.mailey@yahoo.co.uk>
2016-09-21 22:50:39 +0100

    add music

--- public/js/game.js
+++ public/js/game.js
@@ -1,24 +1,13 @@
 var game = new Phaser.Game(1000, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var platforms;
+var player;
 
 function preload() {
+  game.load.audio('bgmusic', '../assets/music.mp3' );
   game.load.image('astronaut', '../assets/astronaut.png');
   game.load.image('ground', '../assets/platform.png');
   game.load.image('ledge', '../assets/platform.png');
   game.load.spritesheet('character', '../assets/characterSprite.png', 32, 32);
-}
-
-var platforms;
-var player;
-
-function createPlayer() {
-  player = game.add.sprite(0, game.world.height - 100, 'character');
-  game.camera.follow(player);
-  game.physics.arcade.enable(player);
-  player.body.gravity.y = 400;
-  player.body.bounce.y = 0.2;
-  player.body.collideWorldBounds = true;
-  player.animations.add('left', [21, 23, 23], 10, true);
-  player.animations.add('right', [33, 34, 35], 10, true);
 }
 
 function create() {
@@ -27,26 +16,9 @@ function create() {
   cursors = game.input.keyboard.createCursorKeys();
   createPlayer();
   buildWorld();
-}
 
-function buildWorld() {
-  platforms = game.add.group();
-  platforms.enableBody = true
-  var ledge = platforms.create(400, 220, 'ledge');
-  var ground = platforms.create(0, game.world.height -64, 'ground');
-  ledge.scale.setTo(1,1);
-  ground.scale.setTo(4, 4);
-  ledge.body.immovable = true;
-  ground.body.immovable = true;
-
-  for(i=0; i< 7; i++) {
-    var box =  platforms.create(i * 100, (i*90), 'ledge');
-    box.scale.setTo(.1, 1);
-    box.body.immovable = true;
-    if((i % 2) === 0) {
-      box.body.immovable = false;
-    }
-  }
+  var music = game.add.audio('bgmusic', .1, true);
+  music.play();
 }
 
 function update() {
@@ -68,5 +40,35 @@ function update() {
 
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -270;
+  }
+}
+
+function createPlayer() {
+  player = game.add.sprite(0, game.world.height - 100, 'character');
+  game.camera.follow(player);
+  game.physics.arcade.enable(player);
+  player.body.gravity.y = 400;
+  player.body.bounce.y = 0.2;
+  player.body.collideWorldBounds = true;
+  player.animations.add('left', [21, 23, 23], 10, true);
+  player.animations.add('right', [33, 34, 35], 10, true);
+}
+
+function buildWorld() {
+  platforms = game.add.group();
+  platforms.enableBody = true
+  var ledge = platforms.create(400, 220, 'ledge');
+  var ground = platforms.create(0, game.world.height -64, 'ground');
+  ledge.scale.setTo(1,1);
+  ground.scale.setTo(4, 4);
+  ledge.body.immovable = true;
+  ground.body.immovable = true;
+
+  for(i=0; i< 7; i++) {
+    var box =  platforms.create(i * 100, (i*90), 'ledge');
+    box.scale.setTo(.1, 1);
+    if((i % 2) !== 0) {
+      box.body.immovable = true;
+    }
   }
 }
